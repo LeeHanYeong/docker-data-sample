290e4bef2cfc70bd7dce8006cb340eff712ffe3b
lhy <dev@lhy.kr>
2023-09-07 16:50:13 +0900

    fix

--- Dockerfile
+++ Dockerfile
@@ -1,2 +1,2 @@
 FROM        mysql:8.1
-COPY        init.sql    /docker-entrypoint-initdb.d
+COPY        init.sql    /docker-entrypoint-initdb.d/init.sql
--- init.sql
+++ init.sql
@@ -1,4 +1,3 @@
 create database sampledb;
 create table sampledb.sampletable(id INT(11), name varchar(20));
 insert into sampledb.sampletable (id, name) values (1, 'samplename');
-select * from sampledb.sampletable;
